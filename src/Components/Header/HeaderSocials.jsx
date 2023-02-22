f3dd57b3b148eeeb0dc87b8d5cc6eb0c37d6b673
Shishir <shishirchaurasiya435@gmail.com>
2023-02-23 02:45:59 +0530

    build asdd

--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ import './Header.css'
 
     <>
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/shishir-chaurasiya-b3b7ab234/" rel="noreferrer" target="_blank" ><BsLinkedin /></a>
+        <a href="https://www.linkedin.com/in/shishir-chaurasiya-b3b7ab234" rel="noreferrer" target="_blank" ><BsLinkedin /></a>
         <a href="https://github.com/Shishir435" rel="noreferrer" target="_blank" ><BsGithub /></a>
         <a href="https://instagram.com/_shishir435" rel="noreferrer" target="_blank" ><BsInstagram /></a>
     </div>
