6191d5cc787b9b446bd8c3109460a35a1313bdf6
Shishir <shishirchaurasiya435@gmail.com>
2023-02-27 15:25:24 +0530

    weather app link

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,33 +1,41 @@
 import React from "react";
-import IMG1 from "../../assets/todo.png";
-import IMG2 from "../../assets/tindog.png";
+import IMG1 from "../../assets/Weather-app.png";
+import IMG2 from "../../assets/todo.png";
 import IMG3 from "../../assets/newsLetter.png";
 import IMG4 from "../../assets/my-1st-site.png";
 import IMG5 from "../../assets/drumkit.png";
-import IMG6 from "../../assets/dice.png";
+import IMG6 from "../../assets/tindog.png";
 import "./portfolio.css";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Todo-List",
-    github: "https://github.com/Shishir435/todo",
-    demo: "https://listsv1.onrender.com/",
+    title: "Weather-App",
+    github: "https://github.com/Shishir435/Weather-app",
+    demo: " https://shishir-weather-app.netlify.app/",
   },
   {
     id: 2,
     image: IMG2,
-    title: "Tin-Dog",
-    github: "https://github.com/Shishir435/Tin-dog",
-    demo: "https://shishir435.github.io/Tin-dog/",
+    title: "Todo-List",
+    github: "https://github.com/Shishir435/todo",
+    demo: "https://listsv1.onrender.com/",
   },
+  
   {
     id: 3,
     image: IMG3,
     title: "Newsletter-Signup",
     github: "https://github.com/Shishir435/Newsletter-signup",
     demo: "https://newsletterv1.onrender.com/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Tin-Dog",
+    github: "https://github.com/Shishir435/Tin-dog",
+    demo: "https://shishir435.github.io/Tin-dog/",
   },
   {
     id: 4,
@@ -43,13 +51,6 @@ const data = [
     github: "https://github.com/Shishir435/Drum-Kit",
     demo: "https://shishir435.github.io/Drum-Kit/",
   },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Dice-Game",
-    github: "https://github.com/Shishir435/Dice-game",
-    demo: " https://shishir435.github.io/Dice-game/",
-  },
 ];
 
 function Portfolio() {
@@ -61,35 +62,32 @@ function Portfolio() {
         <div className="container portfolio__conatiner">
           {data.map(({ id, image, title, github, demo }) => {
             return (
-              
-                <article key={id} className="portfolio_item">
-                  <div className="portfolio_item-image">
-                    <img src={image} alt={title} />
-                  </div>
-                  <h3>{title}</h3>
-                  <div className="portfolio_item-cta">
-                    <a
-                      href={github}
-                      className="btn"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Github
-                    </a>
-                    <a
-                      href={demo}
-                      className="btn btn-primary"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Demo
-                    </a>
-                  </div>
-                </article>
-             
+              <article key={id} className="portfolio_item">
+                <div className="portfolio_item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio_item-cta">
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                  <a
+                    href={demo}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </article>
             );
           })}
-          
         </div>
       </section>
     </>
